630847e9e744c4b261d2004a36a8e869d135a5b0
stefano.introini <stefano.introini@valeriopsggmail.onmicrosoft.com>
2024-08-26 15:22:13 +0200

    Aggiunta la verifica dell'email nella registrazione

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verifica l\'indirizzo email')
+                ->greeting('Benvenuto!')
+                ->line('Per favore, clicca il pulsante qui sotto per verificare l\'email.')
+                ->action('Verifica l\'indirizzo email', $url)
+                ->line('Se non hai creato un account, non è richiesta nessuna azione.')
+                ->salutation('Ti auguriamo un buon proseguimento, ' . env('APP_NAME'));
+        });
     }
 }
