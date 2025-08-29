db2b7b1be7eb049f8100b036e67eb9b2b4ec934c
stefano.introini <stefano.introini@valeriopsggmail.onmicrosoft.com>
2025-08-29 18:13:41 +0200

    Impostata la connessione con n8n

---
+++ app/Http/Controllers/WorkflowApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Service;
+
+class WorkflowApiController extends Controller
+{
+    public function getServiceData() {
+        $services = Service::with([
+            'serviceKeywords',
+            'serviceOptions'
+        ])
+        ->where('is_active', true)
+        ->get()
+        ->map(fn ($service) => [
+            "name" => $service->name,
+            "short_description" => $service->short_description,
+            "description" => $service->description,
+            "base_price" => $service->base_price,
+            "is_active" => $service->is_active,
+            "keywords" => $service->serviceKeywords->map(fn ($keyword) => [
+                "name" => $keyword->name,
+                "priority" => $keyword->priority,
+                "is_exclusive" => $keyword->is_exclusive
+            ])->toArray(),
+            "options" => $service->serviceOptions->map(fn ($option) => [
+                "name" => $option->name,
+                "description" => $option->description,
+                "price" => $option->price,
+                "keywords" => explode(', ', $option->keywords),
+                "is_active" => $option->is_active
+            ])->toArray()
+        ])->toArray();
+
+        return response()->json($services);
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\WorkflowApiController;
 
 /*
 Route::middleware('auth', 'admin')->group(function () {
@@ -11,3 +12,8 @@ Route::middleware('auth', 'admin')->group(function () {
 
 Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 Route::get('/getservices', [AdminController::class, 'getServices'])->name('admin.getServices');
+
+// Workflow
+Route::middleware('wokflow.api')->group(function () {
+    Route::get('/api/getservicedata', [WorkflowApiController::class, 'getServiceData'])->name('api.getServiceData');
+});
